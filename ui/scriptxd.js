9d0b8002970810324f2b972159fe3bb2d0d2c2a8
Whelisson Dias <whelidias@hotmail.com>
2021-09-16 14:55:03 -0300

    correção das cores e dos eventos js

--- ui/scriptxd.js
+++ ui/scriptxd.js
@@ -3,32 +3,31 @@
 $(document).ready(function(){
 	window.addEventListener("message",function(event){
     var id = event.data.css+event.data.diferencial;
-    
     if ( event.data.css == 'aviso'){
         $(`ul`).append(` 
-        <li id='${id}' class="animate__animated animate__backInRight sucesso">
-        <i class="fas fa-exclamation-triangle ${event.css}"></i>
-        <p>${event.data.mensagem}</p>
+        <li id='${id}' class="animate__animated animate__backInRight ${event.data.css}">
+        <i class="fas fa-exclamation-triangle ${event.data.css}"></i>
+        <p>${event.data.mensagem} </p>
         </li>
         `)
     }else if(event.data.css == 'sucesso'){
         $(`ul`).append(` 
-        <li id='${id}' class="animate__animated animate__backInRight sucesso">
-        <i class="fas fa-check-circle ${event.css}"></i>
-        <p>${event.data.mensagem}</p>
+        <li id='${id}' class="animate__animated animate__backInRight ${event.data.css}">
+        <i class="fas fa-check-circle ${event.data.css}"></i>
+        <p>${event.data.mensagem} </p>
         </li>
         `)
     }else if(event.data.css == 'negado'){
         $(`ul`).append(` 
-        <li id='${id}' class="animate__animated animate__backInRight sucesso">
-        <i class="fas fa-minus-circle ${event.css}"></i>
-        <p>${event.data.mensagem}</p>
+        <li id='${id}' class="animate__animated animate__backInRight ${event.data.css}">
+        <i class="fas fa-minus-circle ${event.data.css}"></i>
+        <p>${event.data.mensagem} </p>
         </li>
         `)
     }else if(event.data.css == 'importante'){
         $(`ul`).append(` 
-        <li id='${id}' class="animate__animated animate__backInRight sucesso">
-        <i class="fas fa-info-circle ${event.css}"></i>
+        <li id='${id}' class="animate__animated animate__backInRight ${event.data.css}">
+        <i class="fas fa-info-circle ${event.data.css}"></i>
         <p>${event.data.mensagem}</p>
         </li>
         `)
