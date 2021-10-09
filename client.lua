ec6cc55b85f5245a6c1ec8cc87193cbba5d630a7
Whelisson Dias <whelidias@hotmail.com>
2021-10-09 08:18:24 -0300

    finalizado v 1.0

--- client.lua
+++ client.lua
@@ -6,10 +6,57 @@ SendNUIMessage({ css = css,
 mensagem = mensagem, tempo =
 tempo, diferencial = diferencial }) diferencial = diferencial + 1 end)
 
+local timer = 0
+local zancudoTimer = 0
+local thread = false
+local thread2 = false
+RegisterNetEvent("Notify:searchTimer")
+AddEventHandler("Notify:searchTimer",function(css,mensagem,tempo)
+    timer = timer + parseInt(mensagem)
+    if not thread then
+        thread = true
+        CreateThread(function()
+            SendNUIMessage({ nuiMsg = "showSearch",css = "procurado",mensagem = disp_time(timer), tempo = timer, diferencial = 1 })
+            while true do
+                Wait(1000)
+                if timer > 0 then 
+                    timer = timer - 1
+                    SendNUIMessage({ css = "procurado",mensagem = disp_time(timer), tempo = timer, diferencial = 1 })
+                else
+                    thread = false
+                    SendNUIMessage({ css = "procurado",mensagem = disp_time(timer), tempo = timer, diferencial = 1 })
+                    break
+                end
+            end
+        end)
+    end
+end)
 
+function disp_time(time)
+    local days = math.floor(time/86400)
+    local remaining = time % 86400
+    local hours = math.floor(remaining/3600)
+    remaining = remaining % 3600
+    local minutes = math.floor(remaining/60)
+    remaining = remaining % 60
+    local seconds = remaining
+    if (hours < 10) then
+      hours = "0" .. tostring(hours)
+    end
+    if (minutes < 10) then
+      minutes = "0" .. tostring(minutes)
+    end
+    if (seconds < 10) then
+      seconds = "0" .. tostring(seconds)
+    end
+    answer = hours..':'..minutes..':'..seconds
+    return answer
+  end
+  
 -- aviso
 -- importante
 -- sucesso
 -- negado
--- financeiro
 -- procurado
+-- zancudo
+-- financeiro
--- ui/scriptxd.js
+++ ui/scriptxd.js
@@ -1,6 +1,32 @@
+function startTimer(duration) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        $('#timer').html(`${minutes + ":" + seconds}`)
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
 $(document).ready(function(){
 	window.addEventListener("message",function(event){
     var id = event.data.css+event.data.diferencial;
+    if (event.data.nuiMsg) {
+        $(`.left`).append(` 
+        <li id='procurado1' class="animate__animated animate__backInLeft procurado">
+        <i class="fas fa-star procurado"></i>
+        <i class="fas fa-star procurado"></i>
+        <i class="fas fa-star procurado"></i>
+        <i class="fas fa-star procurado"></i>
+        <i class="fas fa-star procurado"></i>
+        <p class="searchTimer"><b>${event.data.mensagem} </b></p>
+        </li>
+        `)
+    }
     if ( event.data.css == 'aviso'){
         $(`.right`).append(` 
         <li id='${id}' class="animate__animated animate__backInRight ${event.data.css}">
@@ -29,34 +55,54 @@ $(document).ready(function(){
         <p>${event.data.mensagem}  </p>
         </li>
         `)
-    }else if(event.data.css == 'procurado'){
+    }else if(event.data.css == 'zancudo'){
+        var duration = 60 * 20; // Converter para segundos
+         // selecionando o timer
+        startTimer(duration); // iniciando o timer
         $(`.left`).append(` 
-        <li id='${id}' class="animate__animated animate__backInLeft ${event.data.css}">
-        <i class="fas fa-star ${event.data.css}"></i>
-        <i class="fas fa-star ${event.data.css}"></i>
-        <i class="fas fa-star ${event.data.css}"></i>
-        <i class="fas fa-star ${event.data.css}"></i>
-        <i class="fas fa-star ${event.data.css}"></i>
-        <p><b>  ${event.data.tempo} </b></p>
+        <li id='${id}' class="animate__animated animate__backInRight ${event.data.css}">
+        <i class="fas fa-user-clock ${event.data.css}"></i>
+        <p>Invasão ao zancudo em: <span id="timer"></span>  </p>
         </li>
         `)
-    }
-
-    setTimeout(function(){
+        /* console.log("aa")
+        if (Number(event.data.tempo2) > 0) {
+            $(`.zancudoTimer`).html(`${event.data.mensagem}`)
+        } else {
+            $(`#${id}`).removeClass("animate__backInRight")
+            $(`#${id}`).addClass(`animate__backOutRight `)
+            setTimeout(function(){
+                $(`#${id}`).remove();   
+            }, 100);
+        } */
+    }else if(event.data.css == 'procurado'){
+        if (Number(event.data.tempo) > 0) {
+            $(`.searchTimer`).html(`${event.data.mensagem}`)
+        } else {
+            $(`#${id}`).removeClass("animate__backInRight")
+            $(`#${id}`).addClass(`animate__backOutRight `)
+            setTimeout(function(){
+                $(`#${id}`).remove();   
+            }, 100);
+        }
         
-        $(`#${id}`).removeClass("animate__backInRight")
-        $(`#${id}`).addClass(`animate__backOutRight `)
-      },event.data.tempo + 2000);
+    }
+    if (event.data.css !== "procurado") {
+        setTimeout(function(){
+            
+                $(`#${id}`).removeClass("animate__backInRight")
+                $(`#${id}`).addClass(`animate__backOutRight `)
+            
+            
+        },event.data.tempo + 2000);
+        setTimeout(function(){
+            $(`#${id}`).remove();   
+        }, event.data.tempo + 2500);
+    }
 
 
 // REMOVE NOTIFICAÇÃO
-setTimeout(function(){
-    // $("#"+event.data.css+"").removeClass("show");
-    /* <div id='${id}' class="exit animate__backOutRight  animate__delay-1s"> */
-    
-    $(`#${id}`).remove();
-    // $("#notifications").removeClass("show");
-  }, event.data.tempo + 2500);
+// 
   })
 });
 
